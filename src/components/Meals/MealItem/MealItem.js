97446045b931c6a5540ad6bbf1aa75eb1571af8b
SrinivasaRaviIdury <raviaahlad@gmail.com>
2021-10-29 11:10:16 +0530

    1 file modified, 4 added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
   const price = `$${props.price}`;
+  console.log(props, price);
   return (
+    // <div></div>
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
       </div>
     </li>
   );

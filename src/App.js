f873620f0734f0efaef4f4ea11c01561447d59b0
SrinivasaRaviIdury <raviaahlad@gmail.com>
2021-10-29 07:52:36 +0530

    2 files modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import './style.css';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 export default function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
